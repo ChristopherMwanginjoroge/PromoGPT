979839c87bc86e061271efd336c7456e5f950b85
Nosh-thee-techy <blessingswanjiku15@gmail.com>
2025-11-13 07:37:14 +0000

    yeah

--- promogpt-frontend/src/api.js
+++ promogpt-frontend/src/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://127.0.0.1:8000", // fallback for local dev
+    ".", // fallback for local dev
   headers: {
     "Content-Type": "application/json",
   },
